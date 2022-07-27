26540975b79d9eb81edcf07428889232983fe892
Daniel <dannysht27@gmail.com>
2022-07-27 13:21:21 +0300

    testpush

---
+++ src/components/page/OrdersPage.jsx
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import LoadingContainer from '../common/LoadingContainer';
+import OrdersTable from '../OrdersTable';
