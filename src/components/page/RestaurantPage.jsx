4d49b4ba1fe7ce9b48eb0f3448c3621cf009fb15
GerganaAngelova02 <gangelova0217@gmail.com>
2022-08-01 13:30:09 +0300

    fix create button

--- src/components/page/RestaurantPage.jsx
+++ src/components/page/RestaurantPage.jsx
@@ -44,7 +44,36 @@ export default function RestaurantPage() {
     setLogoSrc(rest);
   }, [values.affinity]);
 
-  const onFormSubmit = () => toggleCharacterModal(true);
+  async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        // 'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: data, // body data type must match "Content-Type" header
+    });
+    
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  const onFormSubmit = () => {
+    const formData = new FormData();
+
+    formData.append('name', values.name);
+    formData.append('address', values.address);
+    formData.append('delivery_price', values.deliveryPrice);
+    formData.append('opening', values.openingHours);
+    formData.append('closing', values.closingHours);
+
+    postData('http://localhost:5000/restaurants', formData);
+  };
 
   const onModalClose = () => {
     toggleCharacterModal(false);
