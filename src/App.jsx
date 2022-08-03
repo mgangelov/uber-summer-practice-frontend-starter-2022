bbcb1c519e11bc53a776a484ed1e36d59db0e4c2
vankata-129 <ivanstoyanchev@abv.bg>
2022-08-03 15:26:18 +0300

    add token authentication and redirect to open orders

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import './App.css';
 import {
   Container, Row,
 } from 'react-bootstrap';
+// import { CookiesProvider } from 'react-cookie';
 import AppNavBar from './components/navigation/AppNavBar';
 
 function App() {
   return (
-    <Container className="App">
-      <Row className="header">
-        <AppNavBar />
-      </Row>
-    </Container>
+      <Container className="App">
+        <Row className="header">
+          <AppNavBar />
+        </Row>
+      </Container>
   );
 }
 
